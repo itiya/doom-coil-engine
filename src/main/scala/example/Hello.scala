995dc7b192fd16c84047deabafaabcaa299878a1
itiya <fragment.vertex@gmail.com>
2018-03-04 17:25:00 +0900

    [add] confファイル読み込み機構の追加

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,6 +1,14 @@
 package example
 
+case class DoomConfiguration(doom: String)
+
 object Hello extends Greeting with App {
+  pureconfig.loadConfig[DoomConfiguration] match {
+    case Right(conf) =>
+      println("doom conf: " + conf.doom)
+    case Left(_) =>
+      println("doom conf read failed.")
+  }
   println(greeting)
 }
 
