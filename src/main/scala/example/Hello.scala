dfe7d12686bb63e480323f6f81cba09198e6958f
itiya <fragment.vertex@gmail.com>
2018-03-04 18:22:10 +0900

    [add] 外部ファイルからconfを読める

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,13 +1,16 @@
 package example
 
+import java.nio.file._
+
 case class DoomConfiguration(doom: String)
 
 object Hello extends Greeting with App {
-  pureconfig.loadConfig[DoomConfiguration] match {
+  val confFiles = Set(Paths.get("./application.conf"))
+  pureconfig.loadConfigFromFiles[DoomConfiguration](confFiles) match {
     case Right(conf) =>
-      println("doom conf: " + conf.doom)
+      println("doom conf from file: " + conf.doom)
     case Left(_) =>
-      println("doom conf read failed.")
+      println("doom conf from file read failed.")
   }
   println(greeting)
 }
