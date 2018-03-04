8d0f7865a13087cd75fc0b4e9eab689b7e6391cd
itiya <fragment.vertex@gmail.com>
2018-03-04 18:41:15 +0900

    [add] 指定したconfファイルが存在しなかった場合にエラーメッセージを表示する

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -2,17 +2,25 @@ package example
 
 import java.nio.file._
 
+import scala.util.{Failure, Success, Try}
+
 case class DoomConfiguration(doom: String)
 
 object Hello extends Greeting with App {
   val confFiles = Set(Paths.get("./application.conf"))
-  pureconfig.loadConfigFromFiles[DoomConfiguration](confFiles) match {
-    case Right(conf) =>
-      println("doom conf from file: " + conf.doom)
-    case Left(_) =>
-      println("doom conf from file read failed.")
+
+  Try(pureconfig.loadConfigFromFiles[DoomConfiguration](confFiles)) match {
+    case Success(configResult) =>
+      configResult match {
+        case Right(conf) =>
+          println("doom conf from file: " + conf.doom)
+        case Left(_) =>
+          println("doom conf from file read failed.")
+      }
+      println(greeting)
+    case Failure(e) =>
+      println("config load failed: " + e.getMessage)
   }
-  println(greeting)
 }
 
 trait Greeting {
