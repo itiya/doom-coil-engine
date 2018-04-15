6d2b15f847973e179a4e35e98705f2a55dbd1e22
itiya <fragment.vertex@gmail.com>
2018-04-15 22:22:46 +0900

    [add] ChannelBreakoutに建玉が無い時の処理を追加

--- src/main/scala/domain/trade/ChannelBreakoutTrade.scala
+++ src/main/scala/domain/trade/ChannelBreakoutTrade.scala
@@ -3,6 +3,7 @@ package domain.trade
 import domain.Position
 import domain.candle.{Candle, CandleSpan}
 import domain.client.FinancialCompanyClient
+import domain.client.order.Side
 import domain.client.order.Side.{Buy, Sell}
 import domain.client.order.logic.OrderWithLogic
 import domain.client.order.logic.OrderWithLogic.Stop
@@ -20,70 +21,83 @@ trait ChannelBreakoutTrade extends TradeLogic {
   protected[this] val offset: Double
   protected[this] val updateSec: Int
   protected[this] val heartbeatCount: Int
+  var count = 0
 
   def trade(): Unit = {
-    var count = 0
     while (true) {
       val (candles, orders, positions) = getInfo
       val highAndLow = calcHighAndLow(candles)
       val positionSum = positions.foldLeft(0.0)((z, n) => z + n.size)
-      val positionSide = positions.head.side
-      orders.headOption.fold
-      {
-        positionSide match {
-          case Buy =>
-            postOrder(Stop(Sell, (highAndLow.low - offset).toInt, size * 2))
-            println("create buy")
-          case Sell =>
-            postOrder(Stop(Buy, (highAndLow.high + offset).toInt, size * 2))
-            println("create sell")
-        }
-        ()
+      positions.headOption.fold{
+
+        cancelOrders()
+        postOrder(Stop(Sell, (highAndLow.low - offset).toInt, size))
+        postOrder(Stop(Buy, (highAndLow.high + offset).toInt, size))
+
+        notify(positionSum, None)
       }
-      {
-        case stop: Stop =>
-          if (stop.price != 0.0) {
-            stop.side match {
-              case Buy =>
-                if (stop.price > highAndLow.high + offset) {
-                  cancelOrders()
-                  postOrder(Stop(Buy, (highAndLow.high + offset).toInt, size * 2))
-                  println(stop.price)
-                  println(highAndLow.high)
-                  println("update buy")
-                }
-              case Sell =>
-                if (stop.price < highAndLow.low - offset) {
-                  cancelOrders()
-                  postOrder(Stop(Sell, (highAndLow.low - offset).toInt, size * 2))
-                  println("update sell")
-                }
-            }
+      { position =>
+        val positionSide = position.side
+        orders.headOption.fold
+        {
+          positionSide match {
+            case Buy =>
+              postOrder(Stop(Sell, (highAndLow.low - offset).toInt, size * 2))
+              println("create buy")
+            case Sell =>
+              postOrder(Stop(Buy, (highAndLow.high + offset).toInt, size * 2))
+              println("create sell")
           }
-        case _ => throw new IllegalArgumentException("規定の注文以外が入っています")
-      }
-
-      if (count < heartbeatCount) {
-        count = count + 1
-      } else {
-        val collateralInfo = companyClient.getCollateral match {
-          case Right(collateral) => collateral.toString + "円だよっ！"
-          case Left(_) => "取得に失敗しちゃった……"
+          ()
         }
-        val side = positionSide match {
-          case Buy => "買い"
-          case Sell => "売り"
+        {
+          case stop: Stop =>
+            if (stop.price != 0.0) {
+              stop.side match {
+                case Buy =>
+                  if (stop.price > highAndLow.high + offset) {
+                    cancelOrders()
+                    postOrder(Stop(Buy, (highAndLow.high + offset).toInt, size * 2))
+                    println(stop.price)
+                    println(highAndLow.high)
+                    println("update buy")
+                  }
+                case Sell =>
+                  if (stop.price < highAndLow.low - offset) {
+                    cancelOrders()
+                    postOrder(Stop(Sell, (highAndLow.low - offset).toInt, size * 2))
+                    println("update sell")
+                  }
+              }
+            }
+          case _ => throw new IllegalArgumentException("規定の注文以外が入っています")
         }
-        val positionInfo = "建玉は" + "%.3f".format(positionSum) + "、" + side + "で建ててるよ！"
-
-        notifier.notify(
-          NotifyMessage("はーとびーとだよ！", Seq(Topic("評価額", collateralInfo), Topic("建玉", positionInfo))),
-          Info
-        )
-        count = 0
+        notify(positionSum, Some(positionSide))
       }
 
       Thread.sleep(updateSec * 1000)
+    }
+  }
+
+  private[this] def notify(positionSum: Double, positionSide: Option[Side]): Unit = {
+    if (count < heartbeatCount) {
+      count = count + 1
+    } else {
+      val collateralInfo = companyClient.getCollateral match {
+        case Right(collateral) => collateral.toString + "円だよっ！"
+        case Left(_) => "取得に失敗しちゃった……"
+      }
+      val side = positionSide.fold("無し"){
+        case Buy => "買い"
+        case Sell => "売り"
+      }
+      val positionInfo = "建玉は" + "%.3f".format(positionSum) + "、" + side + "で建ててるよ！"
+
+      notifier.notify(
+        NotifyMessage("はーとびーとだよ！", Seq(Topic("評価額", collateralInfo), Topic("建玉", positionInfo))),
+        Info
+      )
+      count = 0
     }
   }
 
