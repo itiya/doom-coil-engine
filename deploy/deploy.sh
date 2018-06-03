dfc7cfa2f8e589444c438e5219fe9d65dd9eb442
itiya <fragment.vertex@gmail.com>
2018-06-04 08:33:17 +0900

    [fix] デプロイスクリプトにsbt assemblyを追加

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-
-#sftp -b sftp.bat conoha
-#ssh conoha mv doom-coil-engine-assembly-0.1.0-SNAPSHOT.jar btcbot
+cd $(dirname ${BASH_SOURCE:-$0}) # move script dir
+cd ../
+sbt assembly
+cd deploy
+sftp -b sftp.bat conoha
+ssh conoha mv doom-coil-engine-assembly-0.1.0-SNAPSHOT.jar btcbot
 ssh conoha cd btcbot \&\& "nohup java -jar doom-coil-engine-assembly-0.1.0-SNAPSHOT.jar > nohup.out 2>&1 < /dev/null &"
